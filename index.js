401accb12d7d879a0b9e80cccb0f085ac347435a
13angs <13angz.um@gmail.com>
2022-06-18 07:32:15 +0700

    Api format

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-// run `node index.js` in the terminal
+const prev_version = 'v1';
+const latest_version = 'v2';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const api = {
+  user: `${latest_version}/user`,
+};
+
+class API {
+  detail() {}
+  list() {}
+  post() {}
+  put() {}
+  delete() {}
+}
+
+class UserApi extends API {
+  list() {
+    return api.user;
+  }
+
+  detail(args) {
+    return api.user + args;
+  }
+}
+
+const apiTest = new UserApi();
+
+console.log(apiTest.detail(' with args'));
+console.log(apiTest.list());
