45c6b4d9e9ed0497e8433edaa6e0b046efb25852
13angs <13angz.um@gmail.com>
2022-06-30 07:50:38 +0000

    pass obj to function

--- basic/function.js
+++ basic/function.js
@@ -1,12 +1,49 @@
-function request(params){
-    console.log(params);
+const routes = {
+    product: '/product'
+}
+
+class API {
+    /**
+     * ### Get all something
+     * config:
+     * - params:
+     *   - id
+     *   - filter
+     * - body:
+     *   - name
+     * - formData: FormData
+     */
+    request(config, url=routes.product){
+        const {params, body, formData} = config;
+        const {id, filter} = params;
+        const {name} = body;
+        
+        // the rest of code here
+        console.log(id, filter, name, formData);
+    }
+
+    prevRequest(id, name, filter){
+        console.log(id);
+        console.log(name);
+        console.log(filter);
+    }
 }
 
 
-request(
-    {
+
+const formData = new API();
+
+const obj = {
+    params: {
+        filter: 'is',
         id: '1',
+    },
+    body: {
         name: 'don',
-        last: 'um'
-    }
-)
+        categoryId: 'kdlsjfla'
+    },
+    formData: formData
+}
+
+const api = new API();
+api.request(obj)
