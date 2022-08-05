b6b1d94f4378e578951a30bf866908b7976e64e4
Has12Wan34 <rbae1998@gmail.com>
2022-08-05 07:44:20 +0000

    wan

---
+++ basic/sort-data/v2.js
@@ -0,0 +1,13 @@
+// const arr = [2, 6, 1, 7, 10, 9, 3, 8, 5, 4];
+
+// function calculate () {
+//     let = i, resulf
+//     for(i=0; i < arr.length - 1 ; i++){ //i=0; i<size; ++i
+//         if(arr[i] > arr[i+1]){
+//             resulf = arr[i]
+//             arr[i] = arr[i+1]
+//             arr[i+1] = resulf
+//         }
+//     }
+//     return arr;
+// }
